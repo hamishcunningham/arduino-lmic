9151ec572796f041d933e7bd5538037fcae2157d
Hamish Cunningham <hamish@gate.ac.uk>
2022-11-30 16:25:08 +0000

    rename IOExpander to unPhoneTCA

--- src/hal/getpinmap_catena4618.cpp
+++ src/hal/getpinmap_catena4618.cpp
@@ -2,18 +2,18 @@
 
 // DON'T EDIT FROM HERE TO "END"
 // (this stuff is injected to support unPhone's TCA9555 IO expander chip)
-#ifndef IO_EXPANDER_INJECTED
-#define IO_EXPANDER_INJECTED
+#ifndef UNPHONE_TCA_INJECTED
+#define UNPHONE_TCA_INJECTED
 #include <Wire.h>  // (we need digitalRead etc. defined before redefining)
-class IOExpander { // this is an excerpt of the full definition that...
+class unPhoneTCA { // this is an excerpt of the full definition that...
 public:            // ...just declares the injected methods
   static void pinMode(uint8_t pin, uint8_t mode);
   static void digitalWrite(uint8_t pin, uint8_t value);
   static uint8_t digitalRead(uint8_t pin);
 };
-#define digitalWrite IOExpander::digitalWrite // call...
-#define digitalRead  IOExpander::digitalRead  // ...ours...
-#define pinMode      IOExpander::pinMode      // ...please
+#define digitalWrite unPhoneTCA::digitalWrite // call...
+#define digitalRead  unPhoneTCA::digitalRead  // ...ours...
+#define pinMode      unPhoneTCA::pinMode      // ...please
 #endif
 // END -- normal library code follows
 
